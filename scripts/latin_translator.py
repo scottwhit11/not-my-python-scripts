fe8d45e422c07a8b2180e5551f27e1c055a6ad4b
Scott Whitfield <scott.whit.11@gmail.com>
2022-07-18 13:03:54 -0700

    create 2 short scripts

---
+++ scripts/latin_translator.py
@@ -0,0 +1,19 @@
+# latin_translator.py 
+
+#request user for input
+user_input = input("Input text to translate to pig latin: ")
+print("User Text: ", user_input)
+
+# This step breaks the words into a list
+updated_user_input = user_input.split(' ')
+
+for j in updated_user_input:
+ if len(j) >= 3: #only translate words with more than 3 characters
+  j = j + "%say" % (j[0]) 
+  j = j[1:]
+  print(j)
+ else:
+  pass
+
+  #To execute from the terminal, run the following code:
+  #python3 latin_translator.py
